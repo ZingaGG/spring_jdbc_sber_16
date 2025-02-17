0e4fd56e12c7ca5f9842fd4cd728691b2c902871
ZingaGG <trad_bar@mail.ru>
2025-02-17 14:54:33 +0300

    TransactionManagment and JpaRepositories enabled

--- src/main/java/org/example/Config/AppConfig.java
+++ src/main/java/org/example/Config/AppConfig.java
@@ -3,13 +3,20 @@ package org.example.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "org.example")
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "org.example.Repository")
 public class AppConfig {
 
     @Bean
@@ -23,7 +30,24 @@ public class AppConfig {
     }
 
     @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource){
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("org.example");
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        return em;
+    }
+
+    @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+        if (entityManagerFactory == null || entityManagerFactory.getObject() == null) {
+            throw new IllegalStateException("EntityManagerFactory is not initialized properly!");
+        }
+        return new JpaTransactionManager(entityManagerFactory.getObject());
     }
 }
