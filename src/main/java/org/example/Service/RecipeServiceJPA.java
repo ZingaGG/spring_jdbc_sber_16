93be4bcb29e15781e0fd0249d9da237acbfea486
ZingaGG <trad_bar@mail.ru>
2025-02-17 14:55:03 +0300

    Service for Recipes realized

--- src/main/java/org/example/Service/RecipeServiceJPA.java
+++ src/main/java/org/example/Service/RecipeServiceJPA.java
@@ -1,17 +1,53 @@
 package org.example.Service;
 
+
 import lombok.RequiredArgsConstructor;
 import org.example.DAO.RecipeDAOEntityManager;
 import org.example.Model.Recipe;
+import org.example.Repository.IRecipeRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class RecipeServiceJPA {
+public class RecipeServiceJPA{
 
+    private final IRecipeRepository iRecipeRepository;
     private final RecipeDAOEntityManager recipeDAOEntityManager;
 
-    public Recipe getRecipeByID(Long id){
-        return recipeDAOEntityManager.getRecipeById(id).orElseThrow(RuntimeException::new);
+    @Transactional
+    public Recipe getRecipeByID(long id){
+        return recipeDAOEntityManager.getRecipeById(id)
+                .orElseGet(() -> {
+            System.out.println("The Recipe does not exist!");
+            return null;
+        });
+    }
+
+    @Transactional
+    public void saveRecipe(Recipe recipe){
+        recipeDAOEntityManager.save(recipe);
+    }
+
+    @Transactional
+    public void deleteRecipeByID(long id){
+        recipeDAOEntityManager.deleteById(id);
+    }
+
+    @Transactional
+    public void updateRecipe(Recipe recipe){
+        iRecipeRepository.save(recipe);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Recipe> searchRecipesByName(String name) {
+        return iRecipeRepository.findByName(name);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Recipe> searchRecipesByNameContaining(String name) {
+        return iRecipeRepository.findByNameContaining(name);
     }
 }
