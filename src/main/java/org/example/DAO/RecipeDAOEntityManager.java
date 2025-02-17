56da0b421207dbeaa393d3392d1817ed99f7b619
ZingaGG <trad_bar@mail.ru>
2025-02-17 14:54:47 +0300

    EntityManager

--- src/main/java/org/example/DAO/RecipeDAOEntityManager.java
+++ src/main/java/org/example/DAO/RecipeDAOEntityManager.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.example.Model.Recipe;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -13,8 +14,24 @@ public class RecipeDAOEntityManager {
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Transactional
     public Optional<Recipe> getRecipeById(Long id){
         Recipe recipe = entityManager.find(Recipe.class, id);
-        return Optional.of(recipe);
+        return Optional.ofNullable(recipe);
+    }
+
+    @Transactional
+    public void save(Recipe recipe) {
+        entityManager.persist(recipe);
+        entityManager.joinTransaction();
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        Recipe recipe = entityManager.find(Recipe.class, id);
+        if (recipe != null) {
+            entityManager.remove(recipe);
+            System.out.println("Recipe deleted!");
+        }
     }
 }
