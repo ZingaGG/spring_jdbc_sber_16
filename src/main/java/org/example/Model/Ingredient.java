428a8ca7e9a5cbc745417822ebc48cbb844aacc3
ZingaGG <trad_bar@mail.ru>
2025-02-17 14:54:11 +0300

    Model adjusted for work with JPA, Hibernate

--- src/main/java/org/example/Model/Ingredient.java
+++ src/main/java/org/example/Model/Ingredient.java
@@ -1,14 +1,26 @@
 package org.example.Model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "ingredients")
 public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "ingredient_name")
     private String name;
+
     private double quantity;
 
     public Ingredient(String name, double quantity) {
--- src/main/java/org/example/Model/Recipe.java
+++ src/main/java/org/example/Model/Recipe.java
@@ -1,6 +1,8 @@
 package org.example.Model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -9,10 +11,24 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "recipes")
 public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    @JoinColumn(name = "recipe_id", nullable = false)
     private List<Ingredient> ingredients;
+
+    public Recipe(String name, List<Ingredient> ingredients) {
+        this.name = name;
+        this.ingredients = ingredients;
+    }
 
     public Recipe(long id, String name) {
         this.id = id;
